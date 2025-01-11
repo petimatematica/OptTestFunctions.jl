70256ad5e8be080ad9ca666f0498b6d13c33177d
EmanuelMQueiroz <106535637+EmanuelMQueiroz@users.noreply.github.com>
2025-01-11 16:14:07 -0300

    Update

--- src/Levy.jl
+++ src/Levy.jl
@@ -1,10 +1,7 @@
 # Levy function
 # Reference: MOLGA, Marcin; SMUTNICKI, Czesław. Test functions for optimization needs. Test functions for optimization needs, v. 101, p. 48, 2005.
 
-using ForwardDiff
-
-function levy_fun(x::Vector{<:Real})
-    n = length(x)  
+function levy_fun(x::Vector{<:Real}, n::Int64)
     w = zeros(eltype(x), n)  
     for i = 1:n 
         w[i] = 1.0 + (x[i] - 1.0) / 4.0
@@ -24,17 +21,17 @@ function levy_fun(x::Vector{<:Real})
     return y
 end
 
-function levy_grad(x::Vector{<:Real})
-    n = length(x)
+function levy_grad(x::Vector{<:Real}, n::Int64)
     w = zeros(Float64, n) 
+    
     for i in 1:n
         w[i] = 1 + (x[i] - 1) / 4
     end
 
     g = zeros(Float64, n)
 
-    sin_pi_w1 = sin(π * w[1] + 1)
     w1 = w[1]
+    sin_pi_w1 = sin(π * w1 + 1)
 
     g_1 = 0.5 * π * sin(π * w1) * cos(π * w1)
     g_2 = 0.5 * (w1 - 1) * (1 + 10 * sin_pi_w1^2)
@@ -55,41 +52,36 @@ function levy_grad(x::Vector{<:Real})
     return g
 end
 
-function levy_hess(x::Vector{<:Real})
-    n = length(x)
+function levy_hess(x::Vector{<:Real}, n::Int64)
     H = zeros(n, n)
 
     w1 = 1 + (x[1] - 1) / 4
-    hess1 = (π^2 / 8) * cos(2 * π * w1)  # Certo
-    hess2 = 1 / 8 * (1 + 10 * sin(π * w1 + 1)) + 5 * π / 4 * (w1 - 1) * cos(π * w1 + 1)
-    hess3 = 5 / 2 * π * (w1 - 1) * sin(π * w1 + 1) * cos(π * w1 + 1) + 5 / 4 * π^2 * (w1 - 1)^2 * cos(2 * π * w1 + 2)
-    #H[1, 1] = 1 / 16 * (2 * π^2 * cos(2 * pi))
+    sin_pi_w1 = sin(π * w1 + 1)
+    cos_pi_w1 = cos(π * w1 + 1)
+    hess_11 = (π^2 / 8) * cos(2 * π * w1)
+    hess_12 = 1 / 8 * (1 + 10 * sin_pi_w1^2) + (5 * π / 2) * (w1 - 1) * sin_pi_w1 * cos_pi_w1
+    hess_13 = 5 / 2 * π * (w1 - 1) * sin_pi_w1 * cos_pi_w1 + 5 / 4 * π^2 * (w1 - 1)^2 * cos(2 * π * w1 + 2)
 
-    H[1, 1] = hess1 + hess2 + hess3
+    H[1, 1] = hess_11 + hess_12 + hess_13
 
-    for i in 2:(n - 1)      
-        wn = 1 + (x[n] - 1) / 4
-        d2 = (2 * pi^2 / 16) * (cos(2 * pi * wn)^2 - sin(2 * pi * wn)^2) + (1 / 16)
-        H[i, i] = d2
+    for i in 2:(n - 1)
+        wi = 1 + (x[i] - 1) / 4
+        sin_pi_wi = sin(π * wi + 1)
+        cos_pi_wi = cos(π * wi + 1)
+        hess_21 = 1 / 8 * (1 + 10 * sin_pi_wi^2) + (5 * π / 2) * (wi- 1) * sin_pi_wi * cos_pi_wi
+        hess_22 = 5 / 2 * π * (wi - 1) * sin_pi_wi * cos_pi_wi + 5 / 4 * π^2 * (wi - 1)^2 * cos(2 * π * wi + 2)      
+        
+        H[i, i] = hess_21 + hess_22
     end
 
-    wn = 1 + (x[n] - 1) / 4
-    d3 = (2 * pi^2 / 16) * (10 * cos(pi * wn + 1)^2 - 10 * sin(pi * wn + 1)^2) + (1 / 16)
-    H[n, n] = d3
+    xn = x[n]
+    wn = 1 + (xn - 1) / 4
+    sin_2pi_wn = sin(2 * π * wn)
+    cos_2pi_wn = cos(2 * π * wn)
+    hess_31 = 1 / 8 * (1 + sin_2pi_wn^2) + 0.5 * π * (wn - 1) * sin_2pi_wn * cos_2pi_wn
+    hess_32 = π / 32 * ((π * xn^2 - 2 * π * xn + π) * cos(4 * π * wn) + 4 * sin_2pi_wn * cos_2pi_wn * (xn - 1))
+    
+    H[n, n] = hess_31 + hess_32
     
     return H
 end
-
-
-#x = [-658.0, 6778.0, 456.0, 456.0, 56.0, 38.0, 67.0]
-#x = [1, 2, 3]
-x = [1.0, 2.0, 3.0]
-#x = [10]
-#y = levy_grad(x) 
-y = levy_hess(x)
-
-#t = ForwardDiff.gradient(levy_fun, x)
-t = ForwardDiff.hessian(levy_fun, x)
-
-println("Algébrico = ", y)
-println("ForwardDiff = ", t)
---
+++ src/Sumpower.jl
@@ -0,0 +1,36 @@
+# Sum of different power function
+# Reference: MOLGA, Marcin; SMUTNICKI, Czesław. Test functions for optimization needs. Test functions for optimization needs, v. 101, p. 48, 2005.
+
+function sumpower_fun(x::Vector{<:Real}, n::Int64)
+    sum = 0
+
+    for i in 1:n 
+        sum += (abs(x[i]))^(i + 1)
+    end
+
+    return sum
+end
+
+function sumpower_grad(x::Vector{<:Real}, n::Int64)
+    g = zeros(Float64, n) 
+    
+    for i in 1:n
+        xi = x[i] 
+        g[i] = (i + 1) * xi * abs(xi)^(i - 1)
+    end
+    
+    return g
+end
+
+function sumpower_hess(x::Vector{<:Real}, n::Int64)
+    H = zeros(Float64, n, n)
+
+    for i in 1:n 
+        xi = x[i]
+        mod_xi = abs(xi)
+        H[i, i] = (i + 1) * mod_xi^(i - 1) + (i + 1) * (i - 1) * xi^2 * mod_xi ^ (i - 3.0)
+    end    
+    
+    return H
+end
+
